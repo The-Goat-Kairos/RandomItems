09f2899f60e3099c83435889edc3c43b18d7298e
Kairos <danielroepke01@gmail.com>
2024-10-23 11:03:39 +0200

    Lekkere countdown

--- src/main/java/com/kairos/randomItems/commands/StartEvent.java
+++ src/main/java/com/kairos/randomItems/commands/StartEvent.java
@@ -4,13 +4,15 @@ import com.kairos.randomItems.RandomItemGiver;
 import com.mojang.brigadier.Command;
 import com.mojang.brigadier.context.CommandContext;
 import io.papermc.paper.command.brigadier.CommandSourceStack;
+import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.minimessage.MiniMessage;
 import net.kyori.adventure.title.Title;
 import org.bukkit.Bukkit;
-import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
 
 import java.time.Duration;
+import java.util.logging.Logger;
 
 @SuppressWarnings("UnstableApiUsage")
 public class StartEvent {
@@ -21,10 +23,29 @@ public class StartEvent {
     }
 
     public static int execute(CommandContext<CommandSourceStack> ctx) {
-        for (Player player : Bukkit.getOnlinePlayers()) {
-            player.sendPlainMessage("EVENEMENT BEGINT!!!");
-        }
-        itemGiver.startEvent();
+        new BukkitRunnable() {
+            int countdown = 5;
+
+            @Override
+            public void run() {
+                itemGiver.plugin.getComponentLogger().debug(Component.text("Oke hij runt?"));
+                if (countdown > 0) {
+                    itemGiver.plugin.getComponentLogger().debug(Component.text("Elke seconde"));
+                    for (Player player : Bukkit.getOnlinePlayers()) {
+                        Component miniMessage = MiniMessage.miniMessage().deserialize("<red><b>" + countdown + "</red></b>");
+                        Title.Times times = Title.Times.times(Duration.ofMillis(10), Duration.ofMillis(800), Duration.ofMillis(180));
+                        Title countdownTitle = Title.title(miniMessage, Component.empty(), times);
+
+                        player.showTitle(countdownTitle);
+                    }
+                    countdown--;
+                } else {
+                    itemGiver.plugin.getComponentLogger().debug(Component.text("Begin event"));
+                    itemGiver.startEvent();
+                    cancel();
+                }
+            }
+        }.runTaskTimer(itemGiver.plugin, 0L, 20L);
 
         return Command.SINGLE_SUCCESS;
     }
